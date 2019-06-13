83515d677f18c8b5476a21eff87d3aec918978c4
Clunt <clunt@foxmail.com>
2019-06-13 15:19:21 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# xg-command-fes
+xg fes pulgin
+
+## Usage
+```shell
+cd $project_dir
+xg fes
+```
+
+### OPTIONS
+`XG_FES="config" xg fes --config-env="XG_FES"`
+`xg fes --config-file="filepath"`
+`XG_FES_REMOTE="url" xg fes --config-remote="XG_FES_REMOTE"`
---
+++ index.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const path = require('path');
+const shell = require('shelljs');
+const version = require('./package.json').version;
+
+const run = require('./lib/run');
+
+
+exports.name = 'fes';
+exports.desc = 'xg fes pulgin';
+exports.options = {
+  '-v, --version': 'version',
+  '-h, --help': 'print this help message'
+};
+
+exports.run = function(argv, cli, env) {
+  // 显示帮助信息
+  if (argv.h || argv.help) {
+    return cli.help(exports.name, exports.options);
+  }
+
+  if (argv.v || argv.version) {
+    console.log(`xg-command-fes version ${version}`);
+    return;
+  }
+
+  fis.log.info(`xg-command-fes@v${version}`);
+
+  const command = argv['_'][1];
+  if (command) {
+    // init, env, test
+    return typeof run[command] === 'function'
+      ? run[command](argv, cli, env)
+      : fis.log.error('invalid command');
+  }
+
+  return run(argv, cli, env);
+};
