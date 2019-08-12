7729d4593aa90c948a984cff2ad6b6f7d9383b94
Clunt <clunt@foxmail.com>
2019-08-12 19:58:12 +0800

    feat: change log name

--- test/getOption.spec.js
+++ test/getOption.spec.js
@@ -1,10 +1,6 @@
 const getOption = require('../lib/getOption');
 
-// process.env = {
-//     // XG_FES: '{"remote":"https://s3plus.meituan.net/v1/mss_877fd457c4cf425388a58130e2279ae8/file/6680fd2b942782dbef8bc67306481f3a.config"}',
-//     // XG_FES: '{"name": "驼峰业务管理后台", "type": "env"}',
-//     XG_FES_REMOTE: 'https://s3plus.meituan.net/v1/mss_877fd457c4cf425388a58130e2279ae8/file/6680fd2b942782dbef8bc67306481f3a.config',
-// };
+
 getOption({
     configEnv: 'XG_FES',
     configFile: '.xg.fes.config',
